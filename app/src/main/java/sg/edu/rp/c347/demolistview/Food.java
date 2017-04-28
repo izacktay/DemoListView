617c9c79cfb699fbae6208a1092a415b512c1709
15017612 <15017612@myrp.edu.sg>
2017-04-28 11:39:35 +0800

    listview on click, shows toast

---
+++ app/src/main/java/sg/edu/rp/c347/demolistview/Food.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.c347.demolistview;
+
+/**
+ * Created by 15017612 on 28/4/2017.
+ */
+
+public class Food {
+    private String name;
+    private boolean star;
+    public Food(String name, boolean star) {
+        this.name = name;
+        this.star = star;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isStar() {
+        return star;
+    }
+}
+
